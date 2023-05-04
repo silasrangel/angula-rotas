fad2b073969a3fccc0585c970d10f8169a04a33d
Silas Rangel <silasrangel1403@gmail.com>
2023-05-04 17:48:01 -0300

    content creatd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'', component: TitleComponent, pathMatch:'full'},
+  //portifolio
+  //portifolio/1
+  //portifolio/1/abc
+  {path:'portifolio', component: CardComponent, children:[
+  {path:':id', component: CardComponent}, 
+  {path:':id/:token', component: CardComponent}, 
+  ]},
+  {path:'**', redirectTo:''}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import{ IndexModule } from './pages/index/index.module';
+import{ PortifolioModule } from './pages/portifolio/portifolio.module';
+import { MenuComponent } from './shared/menu/menu.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IndexModule,
+    PortifolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
